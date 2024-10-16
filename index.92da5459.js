c06f63b7aaa505e5d7facd2a856cbe29f7e66dee
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-16 20:56:28 +0300

    make build

---
+++ index.92da5459.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(e){var l=e.target.querySelector("ul");"none"!==l.style.display?l.style.display="none":l.style.display="block"});
+//# sourceMappingURL=index.92da5459.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+// write code here
+document.addEventListener("click", (e)=>{
+    const clickedElement = e.target;
+    const list = clickedElement.querySelector("ul");
+    if (list.style.display !== "none") list.style.display = "none";
+    else list.style.display = "block";
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
